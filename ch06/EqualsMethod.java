15ed532fb1843be969182c89440e3ad61604b461
tom0221 <tomuto0221@gmail.com>
2021-04-29 12:02:29 +0900

    ch06ファイル追加

---
+++ ch06/EqualsMethod.java
@@ -0,0 +1,20 @@
+package jp.co.fl.superintro.ch06;
+
+public class EqualsMethod {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//比較用の文字列を管理する変数の宣言と初期化
+		String str = "テスト";
+
+		System.out.println("--if文開始--");
+
+		if(str.equals("テスト")) {
+			System.out.println("String型の変数strと文字列 \"テスト\" とは意味的に等しい。");
+
+		}
+
+		System.out.println("--if文終了--");
+	}
+
+}
